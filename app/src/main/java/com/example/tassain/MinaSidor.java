7f3ea7fb57eef6b7b27433fd049b202aefdcebf6
mli97 <114924508+mli97@users.noreply.github.com>
2023-05-22 14:34:16 +0200

    mina sidor skapad

---
+++ app/src/main/java/com/example/tassain/MinaSidor.java
@@ -0,0 +1,16 @@
+package com.example.tassain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MinaSidor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mina_sidor);
+
+
+    }
+}
--- app/src/main/java/com/example/tassain/logIn.java
+++ app/src/main/java/com/example/tassain/logIn.java
@@ -3,25 +3,32 @@ package com.example.tassain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class logIn extends AppCompatActivity {
 
     EditText editMail, editPassword;
-    Button btnLogInIn;
+    Button btnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        editMail = findViewById(R.id.editEmail);
-        editPassword = findViewById(R.id.editPassword);
+        editMail = findViewById(R.id.emailLogIn);
+        editPassword = findViewById(R.id.passwordLogIn);
 
-        btnLogInIn = findViewById(R.id.btnLogInIn);
+        btnLogIn = findViewById(R.id.btnLogInIn);
 
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
 
+            }
+        });
 
     }
 }
